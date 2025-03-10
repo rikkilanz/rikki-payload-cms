2972f81f6895b7c96267bf7c5fcec7e9fe37d65f
Rikki Soriano <rikkilanz@gmail.com>
2025-03-09 18:11:23 -0700

    Payload setup

---
+++ src/app/(frontend)/layout.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './styles.css'
+
+export const metadata = {
+  description: 'A blank template using Payload in a Next.js app.',
+  title: 'Payload Blank Template',
+}
+
+export default async function RootLayout(props: { children: React.ReactNode }) {
+  const { children } = props
+
+  return (
+    <html lang="en">
+      <body>
+        <h1>Hello world!</h1>
+        <main>{children}</main>
+      </body>
+    </html>
+  )
+}
